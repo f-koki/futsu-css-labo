dc470032901853c703ce9e2f2d7f982b589231f7
f-koki <fukushimakoki0108@gmail.com>
2020-06-05 22:10:37 +0900

    feat

--- src/component/Square/index.tsx
+++ src/component/Square/index.tsx
@@ -3,20 +3,22 @@ import styled from "styled-components";
 
 type Props = {
   color: string;
+  text: string;
 };
 
-const Square: React.FC<Props> = ({ color }) => {
+const Square: React.FC<Props> = ({ color, text }) => {
   return (
     <SquareBox className="Square" color={color}>
-      hoge
+      {text}
     </SquareBox>
   );
 };
 
 const SquareBox = styled.div<{ color?: string }>`
-  width: 64px;
-  height: 64px;
+  width: 158px;
+  height: 158px;
   background-color: ${({ color }) => (color ? color : "blue")};
+  color: white;
 `;
 
 export default Square;
--- src/container/FlexBoxOne/index.tsx
+++ src/container/FlexBoxOne/index.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Square from "../../component/Square";
+import styled from "styled-components";
 
 const FlexBoxOne: React.FC = () => {
   return (
-    <div className="FlexBoxOne">
-      <Square color="red" />
-    </div>
+    <StyledFlexBox className="FlexBoxOne">
+      <Square color="red" text="1" />
+      <Square color="green" text="2" />
+      <Square color="blue" text="3" />
+    </StyledFlexBox>
   );
 };
+
+const StyledFlexBox = styled.div`
+  display: flex;
+  flex-wrap: wrap-reverse;
+`;
 
 export default FlexBoxOne;
