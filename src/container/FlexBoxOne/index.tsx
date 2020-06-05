14f5da6fe6b693b3b8e078ae87fb8fa39cf1bab6
f-koki <fukushimakoki0108@gmail.com>
2020-06-05 22:58:48 +0900

    feat

--- src/container/FlexBoxOne/index.tsx
+++ src/container/FlexBoxOne/index.tsx
@@ -5,16 +5,34 @@ import styled from "styled-components";
 const FlexBoxOne: React.FC = () => {
   return (
     <StyledFlexBox className="FlexBoxOne">
-      <Square color="red" text="1" />
-      <Square color="green" text="2" />
-      <Square color="blue" text="3" />
+      <Square1 color="red" text="1" />
+      <Square2 color="green" text="2" />
+      <Square3 color="blue" text="3" />
+      <Square4 color="black" text="4" />
+      <Square5 color="purple" text="5" />
     </StyledFlexBox>
   );
 };
 
 const StyledFlexBox = styled.div`
   display: flex;
-  flex-wrap: wrap-reverse;
+  flex-flow: row;
+`;
+
+const Square1 = styled(Square)`
+  flex-grow: 1;
+`;
+const Square2 = styled(Square)`
+  flex-grow: 2;
+`;
+const Square3 = styled(Square)`
+  flex-grow: 1;
+`;
+const Square4 = styled(Square)`
+  flex-grow: 2;
+`;
+const Square5 = styled(Square)`
+  flex-grow: 1;
 `;
 
 export default FlexBoxOne;
