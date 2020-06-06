eecd282d2a13e7ba746dcbd553332dfef45250d6
f-koki <fukushimakoki0108@gmail.com>
2020-06-06 22:12:07 +0900

    feat

--- src/component/Square/index.tsx
+++ src/component/Square/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import "./Square.css";
 
 type Props = {
   color: string;
--- src/container/FlexBoxOne/index.tsx
+++ src/container/FlexBoxOne/index.tsx
@@ -5,11 +5,11 @@ import styled from "styled-components";
 const FlexBoxOne: React.FC = () => {
   return (
     <StyledFlexBox className="FlexBoxOne">
-      <Square1 color="red" text="1" />
-      <Square2 color="green" text="2" />
-      <Square3 color="blue" text="3" />
-      <Square4 color="black" text="4" />
-      <Square5 color="purple" text="5" />
+      <Square color="red" text="1"></Square>
+      <Square color="blue" text="2"></Square>
+      <Square color="green" text="3"></Square>
+      <Square color="purple" text="4"></Square>
+      <Square color="black" text="5"></Square>
     </StyledFlexBox>
   );
 };
@@ -17,22 +17,6 @@ const FlexBoxOne: React.FC = () => {
 const StyledFlexBox = styled.div`
   display: flex;
   flex-flow: row;
-`;
-
-const Square1 = styled(Square)`
-  flex-grow: 1;
-`;
-const Square2 = styled(Square)`
-  flex-grow: 2;
-`;
-const Square3 = styled(Square)`
-  flex-grow: 1;
-`;
-const Square4 = styled(Square)`
-  flex-grow: 2;
-`;
-const Square5 = styled(Square)`
-  flex-grow: 1;
 `;
 
 export default FlexBoxOne;
