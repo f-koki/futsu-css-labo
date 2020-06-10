a2cc3b854e353ea844ab61eedbb81a4bff46470c
f-koki <fukushimakoki0108@gmail.com>
2020-06-10 21:24:26 +0900

    feat

--- src/container/GridLayoutNormal/index.tsx
+++ src/container/GridLayoutNormal/index.tsx
@@ -5,18 +5,36 @@ import styled from "styled-components";
 const GridLayoutNormal: React.FC = () => {
   return (
     <StyledGridLayout className="GridLayoutNormal">
-      <Square color="red" text="1"></Square>
-      <Square color="blue" text="2"></Square>
-      <Square color="green" text="3"></Square>
-      <Square color="purple" text="4"></Square>
-      <Square color="black" text="5"></Square>
+      <ItemA>A</ItemA>
+      <ItemB>B</ItemB>
+      <ItemC>C</ItemC>
     </StyledGridLayout>
   );
 };
 
 const StyledGridLayout = styled.div`
-  display: flex;
-  flex-flow: row;
+  display: grid;
+  grid-template-rows: 100px 50px 50px;
+  grid-template-columns: 150px 150px 150px;
+`;
+
+const ItemA = styled.div`
+  grid-row: 1/2;
+  grid-column: 3/4;
+  background-color: purple;
+`;
+
+const ItemB = styled.div`
+  color: white;
+  grid-row: 2;
+  grid-column: 2;
+  background-color: blue;
+`;
+
+const ItemC = styled.div`
+  grid-row: 3/4;
+  grid-column: 1/4;
+  background-color: yellow;
 `;
 
 export default GridLayoutNormal;
